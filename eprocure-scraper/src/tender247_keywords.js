fedb3fc2f3815335ffd35002c2c468f26e786135
Aryan Yadav <yadavaryan466@gmail.com>
2026-02-08 02:25:58 +0530

    tender 247 keyword ids

---
+++ eprocure-scraper/src/tender247_keywords.js
@@ -0,0 +1,79 @@
+export const TENDER247_KEYWORDS = [
+    {
+        "keyword_id": 19015,
+        "product_id": 1031,
+        "industry_id": 53,
+        "sub_industry_id": 163,
+        "keyword_name": "Diesel fuel"
+    },
+    {
+        "keyword_id": 15881,
+        "product_id": 1050,
+        "industry_id": 32,
+        "sub_industry_id": 324,
+        "keyword_name": "Diesel generators"
+    },
+    {
+        "keyword_id": 21108,
+        "product_id": 3956,
+        "industry_id": 41,
+        "sub_industry_id": 327,
+        "keyword_name": "Bitumen Compound"
+    },
+    {
+        "keyword_id": 20460,
+        "product_id": 1902,
+        "industry_id": 41,
+        "sub_industry_id": 327,
+        "keyword_name": "solvent"
+    },
+    {
+        "keyword_id": 19012,
+        "product_id": 1031,
+        "industry_id": 53,
+        "sub_industry_id": 163,
+        "keyword_name": "Kerosene"
+    },
+    {
+        "keyword_id": 19033,
+        "product_id": 1860,
+        "industry_id": 53,
+        "sub_industry_id": 139,
+        "keyword_name": "Propylene"
+    },
+    {
+        "keyword_id": 30321,
+        "product_id": 1698,
+        "industry_id": 53,
+        "sub_industry_id": 139,
+        "keyword_name": "turpentine oil"
+    },
+    {
+        "keyword_id": 1384,
+        "product_id": 1123,
+        "industry_id": 38,
+        "sub_industry_id": 232,
+        "keyword_name": "Sulphur"
+    },
+    {
+        "keyword_id": 31438,
+        "product_id": 3956,
+        "industry_id": 41,
+        "sub_industry_id": 327,
+        "keyword_name": "asphalt"
+    },
+    {
+        "keyword_id": 138,
+        "product_id": 913,
+        "industry_id": 33,
+        "sub_industry_id": 113,
+        "keyword_name": "Asphalting Work"
+    },
+    {
+        "keyword_id": 16580,
+        "product_id": 288,
+        "industry_id": 11,
+        "sub_industry_id": 148,
+        "keyword_name": "Furnaces"
+    }
+]
